6e44c933546d8357e20537c0d0b58d6949054df8
Larabeatriz08 <laradesouzalima08@gmail.com>
2026-05-12 22:06:26 -0300

    dashboard concluido para novo usuario

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,26 @@
 lucide.createIcons();
+
+const userTrigger =
+document.getElementById("userTrigger");
+const dropdown =
+document.getElementById("dropdown");
+
+
+userTrigger.addEventListener("click",()=>{
+
+  dropdown.classList.toggle("active");
+
+});
+
+
+window.addEventListener("click",(e)=>{
+
+  if(
+    !e.target.closest(".user-menu")
+  ){
+
+    dropdown.classList.remove("active");
+
+  }
+
+});
